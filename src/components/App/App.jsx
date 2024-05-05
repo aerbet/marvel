4e2609a62fd065f5894cb6688284b1bfe61ddcb3
Erbol Zhaparov <ae.270718@gmail.com>
2024-05-05 17:32:46 +0600

    completed design for app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,24 @@
+import AppHeader from "../AppHeader/AppHeader.jsx";
+import RandomChar from "../randomChar/RandomChar.jsx";
+import CharList from "../CharList/CharList.jsx";
+import CharInfo from "../CharInfo/CharInfo.jsx";
+
+import decoration from '../../resources/img/vision.png';
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppHeader/>
+            <main>
+                <RandomChar/>
+                <div className="char__content">
+                    <CharList/>
+                    <CharInfo/>
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision"/>
+            </main>
+        </div>
+    )
+}
+
+export default App;
