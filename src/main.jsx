4a2477f079cb07e24354b7c710c3efa7462037ff
Erbol Zhaparov <ae.270718@gmail.com>
2024-05-06 19:04:12 +0600

    added some features, working with api

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
-import MarvelService from "./services/MarvelService";
 
 import './style/style.scss'
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,12 +12,25 @@ class MarvelService {
     return await res.json();
   }
   
-  getAllCharacters = () => {
-    return this.getResource(`${this._apiBase}characters?limit=9&offset=360&${this._apiKey}`);
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=360&${this._apiKey}`);
+    
+    return res.data.results.map(this._transformCharacter)
   }
   
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description === '' ? 'The character does not have description.' : char.description.slice(0, 214) + '...',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    }
+  }
+  
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0])
   }
 }
 
